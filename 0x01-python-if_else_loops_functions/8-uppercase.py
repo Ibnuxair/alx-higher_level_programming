e22c457704a4e6727e9e4aa9162a4866272eaf93
Ibnuxair <ibrahimuzairu28@gmail.com>
2023-08-10 06:50:10 -0700

    Task8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    upper_case = ""
+    for ch in str:
+        ascii_val = ord(ch)
+        if ascii_val >= 97 and ascii_val <= 122:
+            upper_ch = chr(ascii_val - 32)
+            upper_case += upper_ch
+        else:
+            upper_case += ch
+    print(upper_case)
