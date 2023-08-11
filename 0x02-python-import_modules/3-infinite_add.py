087440e76e33407ffa4212fc70b1597967258839
Ibnuxair <ibrahimuzairu28@gmail.com>
2023-08-11 09:50:21 -0700

    Task3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,14 +2,15 @@
 
 import sys
 
-args_len = len(sys.argv)
-i = 1
-result = 0
+if __name__ == "__main__":
+    args_len = len(sys.argv)
+    i = 1
+    result = 0
 
-if args_len == 1:
-    print("{:d}".format(result))
-else:
-    while i < args_len:
-        result += int(sys.argv[i])
-        i += 1
-    print("{:d}".format(result))
+    if args_len == 1:
+        print("{:d}".format(result))
+    else:
+        while i < args_len:
+            result += int(sys.argv[i])
+            i += 1
+        print("{:d}".format(result))
