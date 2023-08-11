b35ccaa47215e9a9f3805023c25ba20d8a20838d
Ibnuxair <ibrahimuzairu28@gmail.com>
2023-08-10 20:54:40 -0700

    Task10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,19 +9,19 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *lion;
-	listint_t *cat;
+	listint_t *tortoise;
+	listint_t *hare;
 
 	if (list == NULL || list->next == NULL)
 		return (0);
 
-	lion = list;
-	cat = list;
-	while (cat != NULL && cat->next != NULL)
+	tortoise = list;
+	hare = list;
+	while (hare != NULL && hare->next != NULL)
 	{
-		lion = lion->next;
-		cat = cat->next;
-		if (lion == cat)
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+		if (tortoise == hare)
 			return (1);
 	}
 	return (0);
