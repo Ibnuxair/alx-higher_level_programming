a0760ba773d41b1303ccfc36397ed2928532df3f
Ibnuxair <ibrahimuzairu28@gmail.com>
2023-08-13 08:34:37 -0700

    Task8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,6 +3,6 @@
 def multiple_returns(sentence):
     sentence_len = len(sentence)
     if sentence_len == 0:
-        return
+        return (sentence_len, None)
     else:
         return (sentence_len, sentence[0])
