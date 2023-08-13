79d0112a5df90845c694ec4f2cab190fe30bb592
Ibnuxair <ibrahimuzairu28@gmail.com>
2023-08-12 19:57:53 -0700

    Task1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 
 def element_at(my_list, idx):
     elements_num = len(my_list)
-    if idx < 0 or idx > elements_num:
+    if idx < 0 or idx >= elements_num:
         return
     else:
         return (my_list.pop(idx))
