d63ca6ce7e04889510478598e445654bd16c5476
Ibnuxair <ibrahimuzairu28@gmail.com>
2023-08-29 13:37:55 -0700

    Task6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+
+"""
+This is a module that defines a class named Square.
+"""
+
+
+class Square:
+    """
+    This class represents a square.
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        """
+        Initialize a square instance with a given size.
+
+        Args:
+            size: The size of the square.
+            position: The position.
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """ Getter to get the size of the square. """
+        return self.__size
+
+    @property
+    def position(self):
+        """ Getter to get the position. """
+        return self.__position
+
+    @size.setter
+    def size(self, size):
+        """ Setter to set new size. """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @position.setter
+    def position(self, value):
+        """ Setter to set the position."""
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+        if not all(isinstance(val, int) and val >= 0 for val in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """
+        Calculate the area of a square.
+
+        Returns:
+               int: The current square area.
+        """
+        return self.size ** 2
+
+    def my_print(self):
+        """ Prints in stdout the square with the character #."""
+        if self.__position is None:
+            raise ValueError("position is not set")
+
+        size = self.__position[0]
+        pos_y = self.__position[1]
+
+        if size == 0:
+            print()
+            return
+
+        for i in range(size):
+            if i < pos_y:
+                print(" " * size)
+            else:
+                print("#" * size)
