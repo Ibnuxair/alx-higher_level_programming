8186ac8bafa4bca85252939c0145da6f474f152e
Ibnuxair <ibrahimuzairu28@gmail.com>
2023-08-30 06:49:08 -0700

    Task2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,18 +1,16 @@
+#!/usr/bin/python3
+
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
+
     try:
         for index in range(x):
             if isinstance(my_list[index], int):
                 print("{:d}".format(my_list[index]), end="")
                 count += 1
-    except IndexError as e:
-        tb = ("Traceback (most recent call last):\n"
-              f'  File "./2-main.py", line 14, in <module>\n'
-              f'    nb_print = safe_print_list_integers(my_list, len(my_list) + 2)\n'
-              f'  File "/0x05/2-safe_print_list_integers.py", line 7, in safe_print_list_integers\n'
-              f'    print("{{:d}}".format(my_list[{index}]), end="")\n'
-              f'IndexError: {e}\n')
-        print(tb)  # Print the custom traceback
-        raise SystemExit(1)  # Terminate the program
+    except (ValueError, IndexError, TypeError) as e:
+        print(e)
+        raise SystemExit(1)
+
     print()
     return count
