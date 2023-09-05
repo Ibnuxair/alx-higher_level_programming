adc8865969cc5d772591b8f22ec58aeb9da9f398
Ibnuxair <ibrahimuzairu28@gmail.com>
2023-09-05 14:06:27 -0700

    Task30

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
+"""
+This is a module that defines a class named LockedClass
+
+It has two lines.
+"""
+
+
 class LockedClass:
-    __slots__ = ('first_name',)
+    """
+    A class representing LockedClass.
+    """
+
+    __slots__ = 'first_name'
