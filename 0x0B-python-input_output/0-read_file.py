1a3725553540275f27649471dd625394e81d3880
Ibnuxair <ibrahimuzairu28@gmail.com>
2023-09-12 08:41:36 -0700

    Readme and Task0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+This module defines a function that reads a text file (UTF8)
+
+and prints it to stdout.
+"""
+
+
+def read_file(filename=""):
+    """Reads a file"""
+
+    with open(filename, encoding="utf-8") as a_file:
+        content = a_file.read().rstrip()
+        print(content)
