ac17c8c50f0aab703b63bbf6ff6b3b776472dd40
Ibnuxair <ibrahimuzairu28@gmail.com>
2023-09-13 08:36:24 -0700

    Task0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,12 +9,10 @@ and prints it to stdout.
 
 def read_file(filename=""):
     """Reads a file and prints it's content."""
-    try:
-        with open(filename, encoding="utf-8") as a_file:
-            content = a_file.read().rstrip()
-            if not content:
-                print("Empty file")
-            else:
-                print(content)
-    except FileNotFoundError:
-        print(f"File not found: {filename}")
+
+    with open(filename, encoding="utf-8") as a_file:
+        content = a_file.read().rstrip()
+        if not content:
+            print("Empty file")
+        else:
+            print(content)
