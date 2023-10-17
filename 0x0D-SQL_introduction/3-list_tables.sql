610ce044debdfba97147afd68a485ae1da38b62e
Ibnuxair <ibrahimuzairu28@gmail.com>
2023-10-17 07:11:01 -0700

    Task3

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,5 +1,3 @@
 -- script that lists all the tables of a database in my MySQL server.
 USE mysql;
-SELECT table_name AS "Tables_in_mysql"
-FROM information_schema.tables
-WHERE table_schema = 'mysql';
+SHOW TABLES;
