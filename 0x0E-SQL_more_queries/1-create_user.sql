6187fb73e59918423b8ec85e6c3d30048fad73e9
Ibnuxair <ibrahimuzairu28@gmail.com>
2023-10-18 10:03:18 -0700

    Task1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,6 @@
 -- a script that creates the MySQL server user user_0d_1
+-- Grant all privileges to user_0d_1
+-- Flush privileges to apply the changes
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
