d69679eebb94f512816da4402f32f068634581bb
Ibnuxair <ibrahimuzairu28@gmail.com>
2023-10-18 19:48:50 -0700

    Task12

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,9 @@
+-- Import the database dump from a file
+source hbtn_0d_tvshows.sql;
+
+-- lists all shows contained in hbtn_0d_tvshows without a genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
