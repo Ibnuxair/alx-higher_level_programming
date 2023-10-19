f2260f5e82959a76d1dee1caf9cedfbdb1ef415d
Ibnuxair <ibrahimuzairu28@gmail.com>
2023-10-19 06:13:42 -0700

    Task10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,9 +1,7 @@
--- Import the database dump from a file
-source hbtn_0d_tvshows.sql;
-
--- List shows with at least one linked genre
+--  lists all shows contained in hbtn_0d_tvshows that have
+-- at least one genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id IS NOT NULL
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, genre_id;
