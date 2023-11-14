bd97f8d76e4fa99d3b66f3b347330de12b9acd3e
Ibnuxair <ibrahimuzairu28@gmail.com>
2023-11-13 19:05:05 -0800

    Task10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,22 +1,18 @@
 #!/usr/bin/node
 
-const factorial = (n) => {
+function factorial(n) {
   if (isNaN(n)) {
     return 1;
   }
 
-  n = parseInt(n);
-
-  if (n < 0) {
-    return "NaN";
-  } else if (n === 0 || n === 1) {
+  if (n === 0 || n === 1) {
     return 1;
   } else {
     return n * factorial(n - 1);
   }
-};
+}
 
 const args = process.argv.slice(2);
-const num = args[0];
+const number = parseInt(args[0]);
 
-console.log(factorial(num));
+console.log(factorial(number));
