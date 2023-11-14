2b03c97c81c22e6497f688bf6f8d4ca83f5b4529
Ibnuxair <ibrahimuzairu28@gmail.com>
2023-11-13 17:53:27 -0800

    Task1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// Using const to create a constant variable
+const line1 = "C is fun";
+const line2 = "Python is cool";
+const line3 = "JavaScript is amazing";
+
+// Printing each line using console.log
+console.log(line1);
+console.log(line2);
+console.log(line3);
