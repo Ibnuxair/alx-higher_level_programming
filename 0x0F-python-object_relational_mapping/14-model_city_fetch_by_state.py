2f031018a4448747675e45dae1b7b5c92566e0dd
Ibnuxair <ibrahimuzairu28@gmail.com>
2023-11-19 13:41:09 -0800

    Task14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
+
 """
-This script deletes State objects with a name containing the letter 'a' from the database hbtn_0e_6_usa.
+This script prints all City objects from the database hbtn_0e_14_usa.
 """
 
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+from model_city import City
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print(
-            "Usage: ./script.py [mysql_username] "
-            "[mysql_password] [database_name]")
+            "Usage: python3 14-model_city_fetch_by_state.py "
+            "<username> <password> <database>")
         sys.exit(1)
 
     username = sys.argv[1]
@@ -24,11 +26,10 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states_to_delete = session.query(State).filter(
-        State.name.ilike('%a%')).all()
+    cities = session.query(State, City).join(
+        City, State.id == City.state_id).order_by(City.id).all()
 
-    for state in states_to_delete:
-        session.delete(state)
-        session.commit()
+    for state, city in cities:
+        print(f"{state.name}: ({city.id}) {city.name}")
 
     session.close()
