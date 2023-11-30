efe16d63fa827698382f3b55553d72adda8998ea
Ibnuxair <ibrahimuzairu28@gmail.com>
2023-11-30 07:29:41 -0800

    Task1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to the URL, displays the body of the response if the status code is 200
-curl -s -w "%{http_code}" -o response.txt "$1" && [[ $(tail -n1 response.txt) == "200" ]] && curl -s -X GET "$1"
+# Takes in a URL, sends a GET request to the URL, and displays the body of the response
+curl -s -X GET "$1"
