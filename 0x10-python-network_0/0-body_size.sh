78a23cf14e20ee5c1ec9a32ad162cb5927ee42fd
Ibnuxair <ibrahimuzairu28@gmail.com>
2023-11-30 06:07:57 -0800

    Task0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script fetches the size of the body in bytes from a given URL using curl
-curl -s -w "%{size_download}" -o /dev/null "$1"
+curl -s -w "%{size_download}\n" -o /dev/null "$1"
