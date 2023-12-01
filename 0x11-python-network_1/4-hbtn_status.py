0c8a6f453084672cc7cb029eb336401a2196b2c8
Ibnuxair <ibrahimuzairu28@gmail.com>
2023-12-01 11:22:22 -0800

    Task4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Fetches content from a URL using requests and displays
+
+the body of the response.
+"""
+
+import requests
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    response = requests.get(url)
+
+    print("Body response:")
+    print(f"\t- type: {type(response.text)}")
+    print(f"\t- content: {response.text}")
